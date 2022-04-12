3d0aeb80c59fb81f83a9e07bcb571e35a863a4cb
Ondina <ondina_patricia@yahoo.com>
2022-04-12 21:42:30 +0300

    Added header&footer in separate files

---
+++ scripts/load_footer.js
@@ -0,0 +1,30 @@
+document.write('' +
+    '<footer>\n' +
+    '    <div class="footer_container">\n' +
+    '        <div class="footer">\n' +
+    '            <div class="footer-heading footer-1">\n' +
+    '                <h1>Discover Signature</h1>\n' +
+    '                <a href="#">About us</a>\n' +
+    '                <a href="#">Newsletter</a>\n' +
+    '                <a href="#">Careers</a>\n' +
+    '                <a href="#">Sign Up</a>\n' +
+    '                <a href="#">FAQ</a>\n' +
+    '                <a href="#">Support</a>\n' +
+    '            </div>\n' +
+    '            <div class="footer-heading footer-2">\n' +
+    '                <h1>Legal</h1>\n' +
+    '                <a href="#">Terms and Conditions</a>\n' +
+    '                <a href="#">Privacy</a>\n' +
+    '                <a href="#">Cookies</a>\n' +
+    '            </div>\n' +
+    '            <div class="footer-heading footer-3">\n' +
+    '                <h1>Socials</h1>\n' +
+    '                <a href="https://www.instagram.com/vaasxo/">Instagram</a>\n' +
+    '                <a href="https://www.facebook.com/vaasxo/">Facebook</a>\n' +
+    '            </div>\n' +
+    '        </div>\n' +
+    '    </div>\n' +
+    '    <div class="copyright_container">\n' +
+    '        <span class="copyright_text">© 2022 Ondina Lipsa, Radu Deleanu, Cristian Pelcear</span>\n' +
+    '    </div>\n' +
+    '</footer>')
---
+++ scripts/load_header.js
@@ -0,0 +1,26 @@
+
+document.write('' +
+    '<header class="menu_bar">\n' +
+    '    <div class="button_and_logo">\n' +
+    '        <a href="homepage.html" class="site-logo">\n' +
+    '            <img src="images/logo.png" alt="logo">\n' +
+    '        </a>\n' +
+    '        <a href="login.html" class="menu_button">Login</a>\n' +
+    '    </div>\n' +
+    '    <nav class="menu">\n' +
+    '        <ul role="menu">\n' +
+    '            <li role="menuitem">\n' +
+    '                <a href="../marketplace.html">Marketplace</a>\n' +
+    '            </li>\n' +
+    '            <li role="menuitem">\n' +
+    '                <a href="../news.html">News</a>\n' +
+    '            </li>\n' +
+    '            <li role="menuitem">\n' +
+    '                <a href="#">About Us</a>\n' +
+    '            </li>\n' +
+    '            <li role="menuitem">\n' +
+    '                <a href="#">Contact</a>\n' +
+    '            </li>\n' +
+    '        </ul>\n' +
+    '    </nav>\n' +
+    '</header>')
