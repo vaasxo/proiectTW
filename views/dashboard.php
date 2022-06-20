d82173486f2f04f99578125d5e021e293b04bbda
mister-big-b <cristoan.pelcear@info.uaic.ro>
2022-06-20 19:10:49 +0300

    update dashboard

--- views/dashboard.php
+++ views/dashboard.php
@@ -3,6 +3,7 @@
  * @var $this View
  */
 
+use core\Application;
 use core\View;
 
 $this->title='Dashboard | Signature'?>
@@ -12,68 +13,30 @@ $this->title='Dashboard | Signature'?>
     <?php include '../public/css/footer.css'; ?>
 </style>
 <body>
+<?php
+$id=Application::$app->session->get('user');
+$my_autograph_ids= \core\Application::$app->db->select('user_autographs',['user_id'=>$id],'autograph_id');
+?>
 <main>
     <span class="title">Your Collection</span>
     <div class="autograph-container">
         <div class="autograph">
-            <img class="autograph__image" src="https://cdn.shopify.com/s/files/1/2030/4039/products/Blanchett_Cate_large.jpg?v=1505155021"
-                 alt="Image failed loading">
-            <h2>Cate Blanchett<br>27 January 2008</h2>
-            <a class="autograph__link" href="/autograph">
-                <p>View autograph</p>
-            </a>
-        </div>
-        <div class="autograph">
+            <?php
 
-            <img class="autograph__image" src="https://xlinegraphics.com/232-large_default/elon-musk-signature.jpg"
-                 alt="Image failed loading">
-            <h2>Elon Musk<br>11 April 2017</h2>
-            <a class="autograph__link" href="/autograph">
-                <p>View autograph</p>
-            </a>
-        </div>
-        <div class="autograph">
+for($i=0; $i < count($my_autograph_ids); $i++) {
+    $my_autographs = \core\Application::$app->db->select('autographs', ['id' => $my_autograph_ids[$i]], '*');
 
-            <img class="autograph__image" src="https://cdn.shopify.com/s/files/1/0101/2750/7515/files/signature-neil-armstrong_medium.jpg?v=1584417716"
-                 alt="Image failed loading">
-            <h2>Neil Armstrong<br>23 February 1987</h2>
-            <a class="autograph__link" href="/autograph">
-                <p>View autograph</p>
-            </a>
-        </div>
-        <div class="autograph">
+    echo "<img class=\"autograph__image\" src=\"../uploaded_images/".$my_autographs[0]['image']."\
+                 alt=\"Image failed loading\">
+            <h2>".$my_autographs[0]['personality']."<br>".$my_autographs[0]['location']."</h2>
+            <a class=\"autograph__link\" href=\"/autograph/".$my_autographs[0]['id']."\">
+            <p>View autograph</p>
+            </a>";
+}
+            ?>
 
-            <img class="autograph__image" src="https://cdn.shopify.com/s/files/1/0101/2750/7515/files/signature-dalai-lama_medium.jpg?v=1584417794"
-                 alt="Image failed loading">
-            <h2>Dalai Lama 14th<br>6 August 1998</h2>
-            <a class="autograph__link" href="/autograph">
-                <p>View autograph</p>
-            </a>
         </div>
-        <div class="autograph">
 
-            <img class="autograph__image" src="https://www.invaluable.com/blog/wp-content/uploads/sites/77/2018/07/famous-signatures-legibility.jpg" alt="Image failed loading">
-            <h2>Andy Warhol<br>July 2018</h2>
-            <a class="autograph__link" href="/autograph">
-                <p>View autograph</p>
-            </a>
-        </div>
-        <div class="autograph">
-
-            <img class="autograph__image" src="https://image.invaluable.com/housePhotos/Novartia/89/722689/H20754-L287632162_original.jpg" alt="Image failed loading">
-            <h2>Pablo Picasso<br>4 February 1965</h2>
-            <a class="autograph__link" href="/autograph">
-                <p>View autograph</p>
-            </a>
-        </div>
-        <div class="autograph">
-
-            <img class="autograph__image" src="https://i.ebayimg.com/images/g/DJ8AAOSwghpe9j7n/s-l1600.jpg" alt="Image failed loading">
-            <h2>Angus Young, Brian Johnson, Malcom Young<br>17 July 2014</h2>
-            <a class="autograph__link" href="/autograph">
-                <p>View autograph</p>
-            </a>
-        </div>
     </div>
 
 </main>
